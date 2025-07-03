71eb4445ac014dd91a26af87c03940dc836e7a4f
Fabio Medeiros <feomedeirosdev@gmail.com>
2025-07-03 09:30:08 -0300

    Feat: automação de criação e processamento de feature store

--- data/tb_general.sql
+++ data/tb_general.sql
@@ -1,6 +1,23 @@
-SELECT 
-   *
+WITH tb_fl_churn AS (
 
-FROM fs_transacoes
+   SELECT 
+      t1.dtRef,
+      t1.idCustomer,
+      -- t2.dtRef AS dtFuturo,
+      -- t2.idCustomer AS idFuturo,
+      CASE WHEN t2.idCustomer IS NULL THEN 1 ELSE 0 END AS flChurn
 
-LIMIT 100
+   FROM fs_general as t1
+
+   LEFT JOIN fs_general AS t2
+   ON t1.idCustomer = t2.idCustomer
+   AND t1.dtRef = date(t2.dtRef, '-28 day')
+
+   WHERE t1.dtRef < date('2024-07-04', '-28 days')
+   AND strftime('%d', t1.dtRef) = '01'
+
+   ORDER BY 1, 2
+
+)
+
+SELECT * FROM tb_fl_churn
